49c24f240994ac2cbbf95391a003281c8dd8566f
DzunN86 <nurroinz@gmail.com>
2022-11-21 11:02:27 +0700

    add assets

--- src/components/BgGradient.vue
+++ src/components/BgGradient.vue
@@ -4,24 +4,18 @@
   >
     <div class="w-[108rem] flex-none flex justify-end">
       <picture
-        ><source
-          srcset="
-            https://tailwindcss.com/_next/static/media/docs@30.8b9a76a2.avif
-          "
-          type="image/avif" />
+        ><source srcset="../assets/docs@30.8b9a76a2.avif" type="image/avif" />
         <img
-          src="https://tailwindcss.com/_next/static/media/docs@30.8b9a76a2.avif"
+          src="../assets/docs@30.8b9a76a2.avif"
           alt=""
           class="w-[71.75rem] flex-none max-w-none dark:hidden"
           decoding="async" /></picture
       ><picture
         ><source
-          srcset="
-            https://tailwindcss.com/_next/static/media/docs-dark@30.1a9f8cbf.avif
-          "
+          srcset="../assets/docs-dark@30.1a9f8cbf.avif"
           type="image/avif" />
         <img
-          src="https://tailwindcss.com/_next/static/media/docs-dark@30.1a9f8cbf.avif"
+          src="../assets/docs-dark@30.1a9f8cbf.avif"
           alt=""
           class="w-[90rem] flex-none max-w-none hidden dark:block"
           decoding="async"
